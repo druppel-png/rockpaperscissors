48dbc4ca1c3f5d14e7af109572e1952b3cb74728
druppel-png <coreyrolton2@gmail.com>
2023-01-31 18:40:29 +1000

    Added check for user input to ensure correct input
    This function takes the user input and checks if it is a null or it is
    not included in the array that contains all possible responses.

--- script.js
+++ script.js
@@ -3,34 +3,39 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if(computerSelection.toUpperCase() == playerSelection.toUpperCase()) {
+    if(computerSelection == playerSelection.toUpperCase()) {
         return 2
     } 
-    else if (computerSelection.toUpperCase() == "SCISSORS" && playerSelection.toUpperCase() == "ROCK") {return 1}
-    else if (computerSelection.toUpperCase() == "PAPER" && playerSelection.toUpperCase() == "SCISSORS") {return 1}
-    else if (computerSelection.toUpperCase() == "ROCK" && playerSelection.toUpperCase() == "PAPER") {return 1}
+    else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "ROCK") {return 1}
+    else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "SCISSORS") {return 1}
+    else if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "PAPER") {return 1}
     else {return 0}
+}
+
+function checkPlayerSelection (playerSelection) {
+    if (playerSelection == null || !myArray.includes(playerSelection.toUpperCase())) {console.log("Please enter a correct response! Restarting the Game."); playGame()}
 }
 
 function playGame() {
     let playerWin = 0;
-    let computerWin= 0;
+    let computerWin = 0;
     let computerSelection;
     let roundResult;
     let playerSelection;
     for (let i = 0; i < 5; i++) {
         computerSelection = getComputerChoice();
-        playerSelection = prompt("Rock, Paper, Scissors?")
+        playerSelection = prompt("Rock, Paper, Scissors?");
+        checkPlayerSelection(playerSelection);
         roundResult = playRound(playerSelection, computerSelection);
         if(roundResult === 1) {playerWin++; console.log("You won this round!")} 
         else if (roundResult === 0) {computerWin++; console.log("You lost this round!")}
         else {console.log("It was a tie!")}
-        console.log("Your choice was " + playerSelection + " and the computer had " + computerSelection + "!")
+        console.log("Your choice was " + playerSelection.toLowerCase() + " and the computer had " + computerSelection.toLowerCase() + "!");
     }
     if (playerWin == computerWin) {return "Tie"}
     else if (playerWin > computerWin) {return "You Win"}
     else {return "You Lost"}
 }
 
-const myArray = ["rock", "paper", "scissors"];
+const myArray = ["ROCK", "PAPER", "SCISSORS"];
 console.log(playGame());
